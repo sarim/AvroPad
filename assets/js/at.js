aae403244ca9988ea8f58a500913d57e0830cdca
Sarim Khan (from nitrous) <sarim2005@gmail.com>
2013-11-24 10:52:57 +0000

    first commit

--- assets/js/at.js
+++ assets/js/at.js
@@ -1,63 +1,17 @@
-var emojis = [
-    "smile", "iphone", "girl", "smiley", "heart", "kiss", "copyright", "coffee",
-    "a", "ab", "airplane", "alien", "ambulance", "angel", "anger", "angry",
-    "arrow_forward", "arrow_left", "arrow_lower_left", "arrow_lower_right",
-    "arrow_right", "arrow_up", "arrow_upper_left", "arrow_upper_right",
-    "art", "astonished", "atm", "b", "baby", "baby_chick", "baby_symbol",
-    "balloon", "bamboo", "bank", "barber", "baseball", "basketball", "bath",
-    "bear", "beer", "beers", "beginner", "bell", "bento", "bike", "bikini",
-    "bird", "birthday", "black_square", "blue_car", "blue_heart", "blush",
-    "boar", "boat", "bomb", "book", "boot", "bouquet", "bow", "bowtie",
-    "boy", "bread", "briefcase", "broken_heart", "bug", "bulb",
-    "person_with_blond_hair", "phone", "pig", "pill", "pisces", "plus1",
-    "point_down", "point_left", "point_right", "point_up", "point_up_2",
-    "police_car", "poop", "post_office", "postbox", "pray", "princess",
-    "punch", "purple_heart", "question", "rabbit", "racehorse", "radio",
-    "up", "us", "v", "vhs", "vibration_mode", "virgo", "vs", "walking",
-    "warning", "watermelon", "wave", "wc", "wedding", "whale", "wheelchair",
-    "white_square", "wind_chime", "wink", "wink2", "wolf", "woman",
-    "womans_hat", "womens", "x", "yellow_heart", "zap", "zzz", "+1",
-    "-1"
-]
-var names = ["Jacob","Isabella","Ethan","Emma","Michael","Olivia","Alexander","Sophia","William","Ava","Joshua","Emily","Daniel","Madison","Jayden","Abigail","Noah","Chloe","你好","你你你"];
-
-var names = $.map(names,function(value,i) {
-    return {'id':i,'name':value,'email':value+"@email.com"};
-});
-var emojis = $.map(emojis, function(value, i) {return {key: value, name:value}});
-
+function range(a,b) {
+    var r = [];
+    for (;a<=b;++a) {
+        r.push(a);
+    }
+    return r;
+}
 $(function(){
-    $inputor = $('#inputor').atwho({
-        at: "@",
-        // data: names,
-        data: "assets/data.json",
-        tpl: "<li data-value='@${name}'>${name} <small>${email}</small></li>",
-        callbacks: {
-            before_save: function(data) {
-                return this.call_default("before_save", data.names);
-            }
-        }
-    }).atwho({
-        at: "#",
-        data: names,
-        tpl: "<li data-value='#${name}'> issues${id} from ${name}</li>"
-    }).atwho({
-        at: ":",
-        data: emojis,
-        tpl:"<li data-value=':${key}:'>${name} <img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${name}.png'  height='20' width='20' /></li>"
-    });
-    $inputor.caret('pos', 47);
-    $inputor.focus().atwho('run');
-
-    $('#editable').atwho({
-        at: ":",
-        data: emojis,
-        tpl:"<li data-value='${key}'>${name} <img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${name}.png'  height='20' width='20' /></li>",
-        insert_tpl: "<img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${name}.png'  height='20' width='20' />",
-        show_the_at: false
-    }).atwho({
-        at: "@",
-        data: names,
-        tpl: "<li data-value='@${name}'>${name}</li>"
-    });
+    $inputor = $('#inputor');
+    range(97,122).concat(range(65,90)).forEach(function(c){
+        $inputor.atwho({
+            at: String.fromCharCode(c),
+            data: 0,
+            tpl:"<li data-value=':${key}:'>${name} <img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${name}.png' height='20' width='20' /></li>"
+        });
+    });    
 });
--- index.md
+++ index.md
@@ -5,26 +5,3 @@ layout: home
 <div>
   <textarea id="inputor" class="inputor">At.js, a github-like autocomplete library :s</textarea>
 </div>
-
-<div id="editable" class="inputor" contentEditable="true">
-  <p>
-    <b>And!!</b> it support <b style="font-size: 20px">ContentEditable</b> mode too!!
-    <img src="http://a248.e.akamai.net/assets.github.com/images/icons/emoji/smile.png" height="20" width="20">
-    <img src="http://a248.e.akamai.net/assets.github.com/images/icons/emoji/smiley.png" height="20" width="20">
-    <img src="http://a248.e.akamai.net/assets.github.com/images/icons/emoji/coffee.png" height="20" width="20">
-  </p>
-  <p>
-    <b>Try here now!</b><img src="http://a248.e.akamai.net/assets.github.com/images/icons/emoji/point_right.png" height="20" width="20">
-     <b>:h</b>
-   </p>
-
-</div>
-
-<!-- <ul class="doc">
-  <li>
-    <strong>Download:</strong> &nbsp;<a href="https://github.com/ichord/At.js/archive/master.zip">v0.3.zip</a>
-  </li>
-  <li>
-    <strong>Support:</strong> &nbsp;<a href="https://github.com/ichord/At.js/wiki">Documentation</a> & <a href="https://github.com/ichord/At.js/issues">Issues</a>
-  </li>
-</ul> -->
